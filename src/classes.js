4b53daf7f2f7d4d6567d8c518c0c380b346a9c2e
Shannon Baker <97621081+sbaker2017@users.noreply.github.com>
2023-08-25 12:06:10 -0500

    Added main and classes framework

---
+++ src/classes.js
@@ -0,0 +1,73 @@
+
+
+// A class to represent each player in the game.
+class Player {
+    constructor () {
+
+    };
+    
+    // Array to hold locations(regions) of each dart
+    dartsThrown = [0, 0, 0];
+    score = 0;
+
+    /*
+    Description: Saves the last score to the database,
+                 Resets the dartsThrown array, and restores
+                 remaining darts.
+    Parameters: 
+    Returns: 
+    */
+    beginTurn = () => {
+
+    };
+
+   
+
+
+}
+
+// A class to represent each leg of the match.
+class Leg {
+    constructor () {
+
+    };
+
+    // Pulls from the database
+    
+
+    /*
+    Description: Takes the values from the thrownDarts array to update
+                 the score for the player.
+    Parameters:
+    Returns:
+    */
+    calculateScore = () => {
+
+    };
+
+
+}
+
+// A class to represent each match.
+class Match {
+    constructor () {
+
+    };
+
+    
+
+
+
+
+
+}
+
+// A class to represent the scoreboard.
+class Scoreboard {
+    constructor () {
+
+    };
+
+    
+
+}
