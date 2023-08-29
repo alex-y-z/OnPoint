036cc43d77c9945e677e4d94cedfb3c8d0205df2
Alex Turner <aturner.xyz@gmail.com>
2023-08-28 23:23:49 -0500

    Flex layout with resizable panels
    The window is divided into two resizable columns: dartboard on the left; scoreboard and statistics on the right.

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1400,
+    height: 950
   });
 
   // and load the index.html of the app.
