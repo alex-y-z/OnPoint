41b879a12a71c00e68d49f1e44ae4e6166b0a204
Alex Turner <aturner.xyz@gmail.com>
2023-09-16 16:12:12 -0500

    Throw recording and board clearing
    For each throw, the throw panel indicates the region ID selected on the board. Additionally, the 'Next Turn' button clears the board/throws.

--- src/scorer.js
+++ src/scorer.js
@@ -4,17 +4,22 @@ const regions = $('g.board-region > path, circle.board-region');
 const throwPanel = $('#throw-panel');
 const throwOptions = $('.throw-dropdown-content > option');
 
-const throws = [];
-
-// Clear board and throw panel
-function clearBoard() {
-
-}
+var throws = [];
 
 // Attach a click listener to each board region
 regions.on('click', (event) => {
+  if (throws.length == 3) {
+    return;
+  }
+
+  // Highlight region
   const region = $(event.target);
-  region.css('fill', 'rgb(0, 238, 255');
+  region.addClass('selected-region');
+
+  // Set throw label
+  throws.push(region.attr('id'));
+  const throwLabel = throwPanel.find(`#throw-label-${throws.length}`);
+  throwLabel.find('button').text(region.attr('name'));
 
   // Replicate the selected region to the spectator
   window.replication.addDart(region.attr('id'), event.clientX, event.clientY);
@@ -39,3 +44,15 @@ const resizeObserver = new ResizeObserver(() => {
 });
 
 resizeObserver.observe(leftPanel.get(0));
+
+// Clear board and throw panel
+$('#next-turn-button').on('click', (event) => {
+  var throwNum = 1;
+  for (regionId of throws) {
+    const region = $(`#${regionId}`);
+    region.removeClass('selected-region');
+    throwPanel.find(`#throw-label-${throwNum}>button`).text('');
+    throwNum++;
+  }
+  throws = [];
+});
