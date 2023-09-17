1cc254796fccab1705544bcc5ff42bfe7a2178e5
Alex Turner <aturner.xyz@gmail.com>
2023-09-17 14:50:27 -0500

    Enhanced throw panel and dart placement
    The throw panel is pretty resilient to any combination of inputs. Additionally, darts are now marked with a placeholder circle.

--- src/scorer.js
+++ src/scorer.js
@@ -5,15 +5,22 @@ const throwPanel = $('#throw-panel');
 const throwOptions = $('.throw-dropdown-content > option');
 
 var throws = [];
+var markers = [];
 var currentThrow = 0;
-var settingThrow = false;
+var changingThrow = null;
 
 // Remove dart
-function removeDart(region) {
+function removeDart(index) {
+  const region = throws[index];
+  const marker = markers[index];
 
-  // Remove highlight if throw is associated with a region
-  if (typeof region === 'object') {
-    region.removeClass('selected-region');
+  if (typeof region === 'object') { // Decrement dart attribute
+    region.attr('data-darts', (_, value) => parseInt(value) - 1);
+    marker.remove();
+
+    if (region.attr('data-darts') == 0) { // Remove highlight
+      region.removeClass('selected-region');
+    }
   }
 }
 
@@ -26,13 +33,31 @@ regions.on('click', (event) => {
   // Highlight region
   const region = $(event.target);
   region.addClass('selected-region');
+  region.attr('data-darts', (_, value) => (value === undefined) && 1 || parseInt(value) + 1);
 
-  // Set throw label
-  throws.splice(currentThrow, 0, region);
-  const throwLabel = throwPanel.find(`#throw-label-${currentThrow}`);
+  // Create placeholder dart
+  let marker = $('<span class="dart-marker"></span>');
+  marker.css('top', event.pageY);
+  marker.css('left', event.pageX);
+  leftPanel.append(marker);
+
+  // Determine throw index
+  let index;
+  if (changingThrow !== null) { // Change a previous throw
+    $(`#throw-label-${changingThrow}`).find('button').removeClass('changing-throw');
+    index = changingThrow;
+    changingThrow = null;
+  }
+  else { // Set the current throw
+    index = currentThrow;
+    currentThrow++;
+  }
+  
+  // Update throw label
+  throws.splice(index, 0, region);
+  markers.splice(index, 0, marker);
+  const throwLabel = throwPanel.find(`#throw-label-${index}`);
   throwLabel.find('button').text(region.attr('name'));
-  currentThrow++;
-  settingThrow = false;
 
   // Replicate the selected region to the spectator
   window.replication.addDart(region.attr('id'), event.clientX, event.clientY);
@@ -44,26 +69,39 @@ throwOptions.on('click', (event) => {
   const dropdown = option.parent().parent();
   const button = dropdown.find('button');
   const index = parseInt(dropdown.attr('data-slot'));
-  const region = throws[index];
 
   // Change an existing dart
   if (option.text() == 'CHANGE') {
-    if (settingThrow || typeof throws[index] === 'undefined') {
-      return; // Already setting or there is no throw to change
+    if (changingThrow || typeof throws[index] === 'undefined') {
+      return; // Already changing or there is no throw to change
     }
 
     button.text('');
-    removeDart(region);
+    button.addClass('changing-throw');
+    removeDart(index);
     throws.splice(index, 1);
-    currentThrow = index;
-    settingThrow = true;
+    markers.splice(index, 1);
+    changingThrow = index;
     return;
   }
 
   // Miss, bounce, or foul
-  button.text(option.text());
-  removeDart(region);
-  throws[index] = option.text();
+  if (changingThrow) {
+    button.removeClass('changing-throw');
+    changingThrow = null;
+  }
+
+  if (index <= currentThrow) {
+    button.text(option.text());
+    removeDart(index);
+    throws[index] = option.text();
+    markers[index] = null;
+
+    // Make this exception count for the current throw
+    if (index == currentThrow) {
+      currentThrow++;
+    }
+  }
 });
 
 // Resize the spectator view to match the scorer view
@@ -76,9 +114,10 @@ resizeObserver.observe(leftPanel.get(0));
 // Clear board and throw panel
 $('#next-turn-button').on('click', (event) => {
   for (index in throws) {
-    removeDart(throws[index]);
+    removeDart(index);
     throwPanel.find(`#throw-label-${index}>button`).text('');
   }
   throws = [];
+  markers = [];
   currentThrow = 0;
 });
