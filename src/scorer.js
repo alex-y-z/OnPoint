f6713e26ad818f0ebcd70f7bebcce8de8ae2d9bf
Shannon Baker <97621081+sbaker2017@users.noreply.github.com>
2023-10-28 18:32:09 -0500

    Fix issue

--- src/scorer.js
+++ src/scorer.js
@@ -567,7 +567,7 @@ function showStatistic(event) {
 
 // Load winner page
 function loadWinner(playerName) {
-  window.replication.showWinner(event, playerName);
+  window.replication.showWinner(playerName);
 
   const modal = $('<iframe id="winner-modal" src="winner.html"></iframe>');
   
